aa25cad5c5be374c4255f52ba3050d0630e46da3
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-05-08 15:10:57 -0400

    Avancement du style du site

--- src/composants/Header.jsx
+++ src/composants/Header.jsx
@@ -1,15 +1,21 @@
-import './Header.scss';
-import React from 'react';
-import './Header';
-import Avatar from '@mui/material/Avatar';
+import "./Header.scss";
+import React from "react";
+import "./Header";
+import Avatar from "@mui/material/Avatar";
 
 function Header() {
-    return (
-        <div className='Header'>
-            <h1>TP2 - React</h1>
-            <Avatar className='avatar' />
-        </div>
-    );
+  return (
+    <div className="Header">
+      <img
+        src="../src/images/amoi_logo.png"
+        alt="logo_a_moi"
+        width={160}
+        height={70}
+      />
+      {/* <h1>TP2 - React</h1> */}
+      <Avatar className="avatar" />
+    </div>
+  );
 }
 
 export default Header;
