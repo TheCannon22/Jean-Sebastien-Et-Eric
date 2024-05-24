80c58e2ba7202a9b1887e125440f1751afd38f96
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-05-24 10:20:07 -0400

    Essai affichage des comics manque restrictions

--- src/code/dossier-modele.js
+++ src/code/dossier-modele.js
@@ -2,54 +2,80 @@
  * ajoute un dossier pour l'utilisateur connecté dans Firestore
  * @param {string} idUtil Identifiant de l'utilisateur
  * @param {object} infoDossier Objet contenant les informations du dossier à ajouter
- * 
+ *
  * @returns {Promise<String>} Identifiant du dossier ajouté
  */
 
-import { collection, getDocs, query, setDoc, doc, deleteDoc, updateDoc } from "firebase/firestore";
-import { bd, collectionUtilisateurs, collectionUtilisateurs } from "./init";
+import {
+  collection,
+  getDocs,
+  query,
+  setDoc,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { bd, collectionUtilisateurs, collectionBandes } from "./init";
 
 export async function creer(idUtil, infoDossier) {
-    const refDossier = doc(collection(bd, collectionUtilisateurs, idUtil, collectionDossiers));
-    console.log("Réference de document nouveau:", refDossier);
-    await setDoc(refDossier, infoDossier);
-    return refDossier.id;
+  const refDossier = doc(
+    collection(bd, collectionUtilisateurs, idUtil, collectionBandes)
+  );
+  console.log("Réference de document nouveau:", refDossier);
+  await setDoc(refDossier, infoDossier);
+  return refDossier.id;
 }
 
 /**
  * Lire *TOUTE* l'info des dossiers de l'utilisateur connecté
- * 
+ *
  * @param {string} idUtil Identifiant de l'utilisateur
- * 
+ *
  * @returns {Promise<array>} Tableau contenant tous les dossiers de cet utilisateur
  */
 
 export async function lireTout(idUtil) {
-   const lesDossiers = await getDocs(query(collection(bd, collectionUtilisateurs, idUtil, collectionDossiers)));
-   console.log("Snapshot contenant les documents dans Firestore: ", lesDossiers.docs);
-   return lesDossiers.docs.map(doc => ({...doc.data(), id: doc.id}));
-}
-
-
-/**
- * 
- * @param {*} idUtil 
- * @param {*} idDossier 
- */
-export async function supprimer(idUtil, idDossier)
-{
-    const refDossier = doc(bd, collectionUtilisateurs, idUtil, collectionDossiers, idDossier);
-    await deleteDoc(refDossier);
+  const lesDossiers = await getDocs(
+    query(collection(bd, collectionUtilisateurs, idUtil, collectionBandes))
+  );
+  console.log(
+    "Snapshot contenant les documents dans Firestore: ",
+    lesDossiers.docs
+  );
+  return lesDossiers.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
 }
 
 /**
- * 
- * @param {*} idUtil 
- * @param {*} idDossier 
- * @param {*} infoDossier 
+ *
+ * @param {*} idUtil
+ * @param {*} idBandes
  */
-export async function modifier(idUtil, idDossier, infoDossier)
-{
-    const refDossier = doc(bd, collectionUtilisateurs, idUtil, collectionDossiers, idDossier);
-    await updateDoc(refDossier, infoDossier);
+export async function supprimer(idUtil, idBandes) {
+  const refDossier = doc(
+    bd,
+    collectionUtilisateurs,
+    idUtil,
+    collectionBandes,
+    idBandes
+  );
+  await deleteDoc(refDossier);
 }
+
+/**
+ *
+ * @param {*} idUtil
+ * @param {*} idBandes
+ * @param {*} infoDossier
+ */
+export async function modifier(idUtil, idBandes, infoDossier) {
+  const refDossier = doc(
+    bd,
+    collectionUtilisateurs,
+    idUtil,
+    collectionBandes,
+    idBandes
+  );
+  await updateDoc(refDossier, infoDossier);
+}
+
+//SectionDesComics.jsx
--- src/composants/SectionDesComics.jsx
+++ src/composants/SectionDesComics.jsx
@@ -1,7 +1,25 @@
 import "./SectionDesComics.scss";
-import React from "react";
+import React, { useEffect } from "react";
+import { lireTout } from "../code/dossier-modele";
 
-function SectionDesComics() {
+export default function SectionDesComics({ setComics }) {
+  useEffect(() => {
+    async function chercherComics() {
+      try {
+        // Chercher toutes les tâches dans Firestore
+        const tousLesComics = await lireTout("userID"); // Pass the user ID here
+        // Raffraîchir l'état React des tâches
+        setComics(tousLesComics);
+        console.log("Data fetched successfully: ", tousLesComics);
+      } catch (error) {
+        console.log("Error fetching data: ", error);
+      }
+    }
+
+    chercherComics();
+    console.log(chercherComics);
+  }, [setComics]);
+
   return (
     <div className="SectionDesComics">
       <h1>Les comics</h1>
@@ -9,5 +27,3 @@ function SectionDesComics() {
     </div>
   );
 }
-
-export default SectionDesComics;
