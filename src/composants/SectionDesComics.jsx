06b959e9d27bb89e0fdacffaf3dba3b7bae149d7
Gabrielle <gabrielle.pelletier.08@gmail.com>
2024-06-04 09:36:03 -0400

    On peut voir les comics un par dessus l'autre

--- src/composants/SectionDesComics.jsx
+++ src/composants/SectionDesComics.jsx
@@ -4,6 +4,7 @@ import { lireTout } from "../code/dossier-modele";
 
 export default function SectionDesComics() {
   const [tousLesComics, setTousLesComics] = useState([]);
+  const [comicSelectionne, setComicSelectionne] = useState(0);
 
   useEffect(() => {
     async function chercherComics() {
@@ -19,12 +20,43 @@ export default function SectionDesComics() {
     chercherComics();
   }, []);
 
+  function afficherDetailsComic(comicIndex) {
+    setComicSelectionne(comicIndex);
+  }
+
+  function afficherComicSuivant() {
+    setComicSelectionne((comicSelectionne + 1) % tousLesComics.length);
+  }
+
+  function afficherComicPrecedent() {
+    setComicSelectionne(
+      (comicSelectionne - 1 + tousLesComics.length) % tousLesComics.length
+    );
+  }
+
+  if (tousLesComics.length === 0) {
+    return <div>Loading...</div>;
+  }
+
+  const comicSelectionneData = tousLesComics[comicSelectionne];
+
+  if (!comicSelectionneData) {
+    return <div>Error: Invalid comic selectionne index.</div>;
+  }
+
   return (
     <div className="SectionDesComics">
       <h1>Les comics</h1>
-      {tousLesComics.map((comic) => (
-        <img src={comic.url} alt={comic.title} key={comic.id} />
-      ))}
+      <div className="DetailsComic">
+        <img src={comicSelectionneData.url} alt={comicSelectionneData.title} />
+        <h2>{comicSelectionneData.title}</h2>
+        <p>{comicSelectionneData.description}</p>
+        <p>Likes: {comicSelectionneData.likes}</p>
+        <p>Comments: {comicSelectionneData.comments}</p>
+        <p>Date: {comicSelectionneData.date}</p>
+        <button onClick={afficherComicPrecedent}>Précédent</button>
+        <button onClick={afficherComicSuivant}>Suivant</button>
+      </div>
     </div>
   );
 }
